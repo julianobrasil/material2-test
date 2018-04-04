ecbbda8c6017c7ae0f2249bf0003ff60d6e92265
Juliano <juliano.brasil@gmail.com>
2018-04-04 07:34:24 -0300

    update angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent, ConfirmationDialog } from './app.component';
+import { AppComponent, ConfirmationDialog, DemoMomentMonthYear } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {
@@ -10,19 +10,39 @@ import {
   MatCardModule,
   MatCheckboxModule,
   MatDialogModule,
+  MatDatepickerModule,
   MatFormFieldModule,
   MatInputModule,
   MatSnackBarModule,
+  MatTabsModule,
 } from '@angular/material';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { MomentDateModule, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'LL',
+  },
+  display: {
+    dateInput: 'LL',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
+
 import 'hammerjs';
+import { CompComponent } from './comp/comp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConfirmationDialog
+    ConfirmationDialog,
+    DemoMomentMonthYear,
+    CompComponent
   ],
   imports: [
     BrowserModule,
@@ -34,15 +54,21 @@ import 'hammerjs';
     MatButtonModule,
     MatCardModule,
     MatCheckboxModule,
+    MatDatepickerModule,
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
     MatSnackBarModule,
+    MatTabsModule,
+
+    MomentDateModule,
 
     FlexLayoutModule
   ],
   entryComponents: [ConfirmationDialog],
-  providers: [],
+  providers: [
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
